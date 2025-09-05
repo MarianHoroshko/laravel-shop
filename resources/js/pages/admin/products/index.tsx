3c0fbd9be46e22bf06e6f0ed9b62e6da49e49827
marian <marian.horoshko@gmail.com>
2025-09-05 10:04:33 +0200

    add favicon

--- resources/js/pages/admin/products/index.tsx
+++ resources/js/pages/admin/products/index.tsx
@@ -141,6 +141,32 @@ export default function ProductsIndex() {
                 </Table>
 
                 <div className="float-end m-2 space-x-0.5">
+                    {/* TODO: make pagination with component */}
+                    {/* <Pagination>
+                        <PaginationContent>
+                            <PaginationItem>
+                                <PaginationPrevious href="#" />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#">1</PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#" isActive>
+                                    2
+                                </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href="#">3</PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationEllipsis />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationNext href="#" />
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination> */}
+
                     {pages.links.length > 0 &&
                         pages.links.map((link, index) => (
                             <Link key={index} href={link.url || '#'}>
