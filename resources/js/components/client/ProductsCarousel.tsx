03296f36b77d38348f39702a8da49b9bb92683ff
marian <marian.horoshko@gmail.com>
2025-09-23 10:24:11 +0200

    display carousel only if have items

--- resources/js/components/client/ProductsCarousel.tsx
+++ resources/js/components/client/ProductsCarousel.tsx
@@ -8,26 +8,30 @@ interface CarouselProps {
 
 export default function ProductsCarousel({ products }: CarouselProps) {
     return (
-        <div className="px-16 py-8">
-            <h2 className="p-8 text-5xl text-primary">Top sales</h2>
+        <>
+            {products !== null && products.length > 0 && (
+                <div className="px-16 py-8">
+                    <h2 className="p-8 text-5xl text-primary">Top sales</h2>
 
-            <Carousel
-                opts={{
-                    align: 'start',
-                }}
-                className="max-w-8xl w-full"
-            >
-                <CarouselContent>
-                    {products.map((product, index) => (
-                        <CarouselItem key={index} className="md:basis-1 lg:basis-1/4">
-                            <ProductCard product={product} />
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
+                    <Carousel
+                        opts={{
+                            align: 'start',
+                        }}
+                        className="max-w-8xl w-full"
+                    >
+                        <CarouselContent>
+                            {products.map((product, index) => (
+                                <CarouselItem key={index} className="md:basis-1 lg:basis-1/4">
+                                    <ProductCard product={product} />
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
 
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
-        </div>
+                        <CarouselPrevious />
+                        <CarouselNext />
+                    </Carousel>
+                </div>
+            )}
+        </>
     );
 }
